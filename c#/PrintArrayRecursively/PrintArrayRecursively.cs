4fd5a907cae08e685486af8557a5c14867acf5a0
brslv <brslvg@gmail.com>
2015-10-05 15:28:42 +0300

    add PrintArrayRecursively problem solution

---
+++ c#/PrintArrayRecursively/PrintArrayRecursively.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// Write a recursive function (use no while loops or for loops)
+// that prints all the elements of an array of integers, one per line.
+// The parameters to the function should be int a[], and int size.
+
+namespace PrintArrayRecursively
+{
+    class PrintArrayRecursively
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[]
+            {
+                5,
+            };
+
+            int arraySize = a.Length;
+
+            Print(a, arraySize);
+        }
+
+        public static void Print(int[] a, int size)
+        {
+            if (size <= 0)
+                return;
+
+            int lastElementIndex = size - 1;
+
+            Print(a, lastElementIndex);
+
+            // Using backwards recursion
+            Console.WriteLine(a[lastElementIndex]);
+        }
+    }
+}
