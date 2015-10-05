71a028c30999a249a779ca00f951f158bb810888
brslv <brslvg@gmail.com>
2015-10-05 15:12:03 +0300

    more informative problem description

--- c#/SumOfDigits/Main.cs
+++ c#/SumOfDigits/Main.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// Write a function with the following prototype that returns the sum of the digits of an integer.
+// Using recursion, write a function with the following prototype that returns the sum of the digits of an integer.
 // int sumOfDigits(int x);
 // If x is 234, the function should return 2 + 3 + 4, that is, 9.
 
